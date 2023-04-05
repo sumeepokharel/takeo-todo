a677d8a88a5fb19b382dbf1dfd9e59df44c496ba
Sumee Pokharel <sumeepokharel@Sumees-MacBook-Pro.local>
2023-04-05 12:00:40 -0500

    connected to database

--- backend/database/database_connection.js
+++ backend/database/database_connection.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 import Users from "./Schema/Users.schema.js";
 
 const connectDatabase = async () => {
-  const connection = await mongoose.connect(
-    `mongodb+srv://bhattaraiyogesh007:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_CLUSTER_NAME}.mongodb.net/?retryWrites=true&w=majority`
-  );
+  const connection = await mongoose.connect(process.env.DB_CONNECTION_URL);
 
   if (connection) {
     console.log("database connected");
